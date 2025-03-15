eb7ed3c481467a4253a1d10f532eecbefaed31d4
KhusiLama <468005khusilama@gmail.com>
2025-03-15 22:42:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('start').addEventListener('click', function() {
+    alert('Sign language interpreter started!');
+    // here you would add the logic to feych and display video content
+});
